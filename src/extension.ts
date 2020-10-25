49a9e2753761bae07a4005648d6dacbe84271182
STR1Z <weihengjiang64@hotmail.com>
2020-10-24 21:00:46 -0400

    added Tree that is not working

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,29 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  vscode.window.registerTreeDataProvider("tree", new TreeDataProvider());
+}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "quick-tagger" is now active!');
+class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
+  data: TreeItem[];
+  constructor() {
+    this.data = [new TreeItem("cars")];
+  }
+  getTreeItem(element: TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
+    return element;
+  }
+  getChildren(element?: TreeItem | undefined): vscode.ProviderResult<TreeItem[]> {
+    if (element === undefined) return this.data;
+    return element.children;
+  }
+}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('quick-tagger.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from quick-tagger!');
-	});
-
-	context.subscriptions.push(disposable);
+class TreeItem extends vscode.TreeItem {
+  children: TreeItem[] | undefined;
+  constructor(label: string, children?: TreeItem[]) {
+    super(label, children === undefined ? vscode.TreeItemCollapsibleState.None : vscode.TreeItemCollapsibleState.Expanded);
+    this.children = children;
+  }
 }
 
 // this method is called when your extension is deactivated
